2d5fde8692dd3f46f67c85020b85153d992af24b
Matthew Baldwin <mbaldw15@gmail.com>
2019-09-02 23:55:35 -0400

    Moved configuration into a new folder for cleanup. Added POC for filtering based on the user having a role.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const auth = require('./auth.json');
-
+const auth = require('../conf/auth.json');
+const consts = require('../conf/constants.json');
 function startup() {
 
   console.log('connecting to discord');
@@ -28,8 +28,21 @@ function processCommand(msg) {
       break;
     case 'uptime':
       msg.channel.send(client.uptime)
+      break;
+    case 'admin':
+      processAdminCommand(args, msg);
+      break;
   }
+}
 
+function processAdminCommand(args, msg) {
+  const allowedRole = msg.guild.roles.find("name", consts.adminRole)
+  const roles = msg.member.roles;
+  if(roles.has(allowedRole.id)) {
+    msg.reply('you have the appropriate role');
+  } else {
+    msg.reply('UNAUTHORIZED');
+  }
 }
 
 startup();
